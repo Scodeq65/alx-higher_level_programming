b757032ef47c37058afb9e195b78734ad140748b
scodeq65 <sodiqagbaraojo@gmail.com>
2023-03-06 20:15:04 -0800

    updated corrections

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str1 += " "+ str2
-print("Welcom to {}!".format(str1))
+str1 += " " + str2
+print("Welcome to {}!".format(str1))
+
