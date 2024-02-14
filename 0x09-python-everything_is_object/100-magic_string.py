3b097e19ec5a9f246643d25a1d034829901251e3
scodeq65 <sodiqagbaraojo@gmail.com>
2024-02-14 13:28:45 -0800

    minor changes

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Initialize a magic_string."""
+
+
 def magic_string():
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return ("Bestschool, " * (magic_string.n - 1) + "Bestschool")
