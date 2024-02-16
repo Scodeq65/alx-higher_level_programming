669910c41b579b94471df1e29b9ff53d659a9b5f
scodeq65 <sodiqagbaraojo@gmail.com>
2024-02-16 04:19:09 -0800

    task 4 done

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,13 +5,15 @@
 
 
 def inherits_from(obj, a_class):
-    """Check if the object inherits (directly or indirectly) from a specified class.
+    """Check if the object inherits (directly or indirectly) from
+        a specified class.
 
     Args:
         obj: The object to be checked.
         a_class: The class to compare against.
 
     Returns:
-        True if obj inherits (directly or indirectly) froam a_class, False otherwise
+        True if obj inherits (directly or indirectly) froam a_class,
+        False otherwise
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is a_class
