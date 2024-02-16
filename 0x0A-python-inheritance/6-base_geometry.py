b5107100af2909009f0a976e65294dcad04acab1
scodeq65 <sodiqagbaraojo@gmail.com>
2024-02-16 06:17:07 -0800

    task 6 done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A class geometry based on previos geometry."""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry."""
+
+    def area(self):
+        """Raises an exception with the message area() is not implemented"""
+        raise Exception("area() is not implement")
