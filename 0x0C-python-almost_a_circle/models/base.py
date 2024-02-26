fb2e1a587ae498ba5e2778d6361497e899fc4483
scodeq65 <sodiqagbaraojo@gmail.com>
2024-02-26 04:41:15 -0800

    task 1 done

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""initialize a class"""
+
+
+class Base:
+    """A new class created."""
+    __nb_objects = 0 # private class attribute
+
+    def __init__(self, id=None):
+        """
+        Initializez a base object.
+
+        Args:
+            id(int): that is the Id of the object if not provided, a new id will be assigned
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
