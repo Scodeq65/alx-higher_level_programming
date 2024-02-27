346e09ce38d57982b7ba4ff037f5893888382c9b
scodeq65 <sodiqagbaraojo@gmail.com>
2024-02-27 08:37:14 -0800

    update public method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,6 +40,25 @@ class Square(Rectangle):
         """
         self.width = value
         self.height = value
+    
+    def update(self, *args, **kwargs):
+        """Updates the attributes of the Square instance.
+        Args:
+            *args: List of arguments.
+            **kwargs: Dictionary of keyword arguments.
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def __str__(self):
         """Returns the strng representation of the square.
