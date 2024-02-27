e62cd253eadf4c105afbc7867a55c839653d60a1
scodeq65 <sodiqagbaraojo@gmail.com>
2024-02-27 07:53:10 -0800

    minor error corrected

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,33 +21,32 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
-        @property
-        def size(self):
-            """Gets the size of the square.
-            Returns:
-                int: The size of the square.
-            """
-            return self.width
+    @property
+    def size(self):
+        """Gets the size of the square.
+        Returns:
+            int: The size of the square.
+        """
+        return self.width
 
-        @size.setter
-        def size(self, value):
-            """Sets the size of the Square.
+    @size.setter
+    def size(self, value):
+        """Sets the size of the Square.
+        Args:
+            value (int): The new size of the square.
+        Raises:
+            TypeError: If value is nnot an integer.
+            ValueError: If value is less than or equal to 0
+        """
+        self.width = value
+        self.height = value
 
-            Args:
-                value (int): The new size of the square.
-            Raises:
-                TypeError: If value is nnot an integer.
-                ValueError: If value is less than or equal to 0
-            """
-            self.width = value
-            self.height = value
+    def __str__(self):
+        """Returns the strng representation of the square.
 
-        def __str__(self):
-            """Returns the strng representation of the square.
-
-            Returns:
-                str: The string representation of the square.
-            """
-            return "[square] ({}) {}/{} - {}".format(
+        Returns:
+            str: The string representation of the square.
+        """
+        return "[square] ({}) {}/{} - {}".format(
                     self.id, self.x, self.y, self.width
-            )
+        )
