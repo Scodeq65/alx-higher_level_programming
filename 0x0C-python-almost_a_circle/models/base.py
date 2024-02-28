a73915132f87402c9728b184b64a3b63bae192f2
Sodiq Agbaraojo <Sodiqagbaraojo@gmail.com>
2024-02-28 17:27:29 +0100

    completed task done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,3 +36,10 @@ class Base:
         filename = cls.__name__ + ".json"
         with open(filename, 'w') as file:
             file.write(cls.to_json_string(list_objs))
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list represented by json_string"""
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return json_loads(json_string)
