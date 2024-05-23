8a82ab741cb13adda476eb12e7911378866322c5
Scodeq65 <sodiqagbaraojo@gmail.com>
2024-05-23 04:37:10 -0700

    advance

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- Script to convert hbtn_0c_0 database, first_table, and name field to UTF8 (utf8mb4)
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
