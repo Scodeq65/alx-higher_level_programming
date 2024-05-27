ec49665b78fda9ef2ec0291249c404e1a7aee242
Scodeq65 <sodiqagbaraojo@gmail.com>
2024-05-27 09:36:52 -0700

    success

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,10 @@
+-- Script that lists all shows contained in database hbtn_0d_tvshows.
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_genres ON id = tv_show_genres.show_id
+ORDER BY
+    tv_shows.title ASC, tv_show_genres.genre_id ASC;
