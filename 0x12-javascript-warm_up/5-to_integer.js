5e79573c69bfa018cb84ae027589b508f229d558
scodeq65 <sodiqagbaraojo@gmail.com>
2024-06-20 00:42:30 -0700

    updated

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,9 +3,8 @@
 const args = process.argv.slice(2);
 const num = parseInt(args[0], 10);
 
-
 if (isNaN(num)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log(`My number: ${num}`);
+  console.log(`My number: ${num}`);
 }
