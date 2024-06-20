df349ca9964f95f7a4780048883076faac01f0df
scodeq65 <sodiqagbaraojo@gmail.com>
2024-06-20 03:44:44 -0700

    7Ups

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 const x = parseInt(args[0], 10);
 
 if (isNaN(x)) {
-  console.log('Missing number of occurences')
+  console.log('Missing number of occurences');
 } else {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
