e369e8e31c189189a0a6dcde1eafe066c2b04e87
scodeq65 <sodiqagbaraojo@gmail.com>
2024-06-20 05:12:25 -0700

    code_lovers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 
 const args = process.argv.slice(2);
 
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
@@ -10,7 +10,7 @@ const a = parseInt(args[0], 10);
 const b = parseInt(args[1], 10);
 
 if (isNaN(a) || isNaN(b)) {
-  console.log('NaN')
+  console.log('NaN');
 } else {
   console.log(add(a, b));
 }
