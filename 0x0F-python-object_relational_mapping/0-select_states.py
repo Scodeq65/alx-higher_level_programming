909c2d1be0fc1e59564cf56b625eb507ad915514
Scodeq65 <sodiqagbaraojo@gmail.com>
2024-06-26 21:05:53 -0700

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
 """
-0-select_states.py - A script that lists all
-states from the database hbtn_0e_0_usa.
+0-select_states.py - Lists all states from the database hbtn_0e_0_usa.
 """
+
 import MySQLdb
 import sys
 
 
 def list_states():
-    """
-    Connects to a MySQL database and lists all states
-    sorted by id in ascending order.
-    """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    try:
-        connection = MySQLdb.connect(
-                host="127.0.0.1",
-                port=3306,
-                user=username,
-                passwd=password,
-                db=db_name
-        )
-        cur = connection.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC LIMIT 5")
-        rows = cur.fetchall()
-
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL: {e}")
-
-    finally:
-        if cur:
-            cur.close()
-        if connection:
-            connection.close()
+    """ Alternative testing code. """
+    username, password, db_name = sys.argv[1:4]
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    conn.close()
 
 
 if __name__ == "__main__":
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+1-filter_states.py - A script that lists all states name
+that starts with 'N' from the database.
+"""
+
+import MySQLdb
+import sys
+
+
+def list_states():
+    """ Cconnects to a MySQL database and lists all states
+    starting with 'N' in ascending order.
+    """
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = conn.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    )
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    list_states()
