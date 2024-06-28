74a19c58a39ec058dc0ee3c8fea3ccccb86b75c3
Scodeq65 <sodiqagbaraojo@gmail.com>
2024-06-28 06:38:17 -0700

    blsesed

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that delete State object 'a' from the database hbtn_0e_6_usa."""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    username, password, db_name = sys.argv[1:4]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost/{db_name}',
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+
+    for state in state_to_delete:
+        session.delete(state)
+    
+    session.commit()
+    session.close()
