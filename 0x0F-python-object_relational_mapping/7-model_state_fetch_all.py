b2bed3eef6cd706388e47a6e6e52f5ac7f87e6dc
Scodeq65 <sodiqagbaraojo@gmail.com>
2024-06-27 18:12:26 -0700

    agba_codder

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Create an engine that stores data in the local directory's hbtn_0e_6_usa database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    
+    # Create a Session
+    session = Session()
+
+    # Query all State objects and order by states.id
+    states = session.query(State).order_by(State.id).all()
+
+    # Print the results
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    # Close the session
+    session.close()
